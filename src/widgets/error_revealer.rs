f094c8f0a7ef381200ab6fd7f405d021b3aa59bf
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-01-31 21:05:10 +0100

    simplify error revealer

--- src/widgets/error_revealer.rs
+++ src/widgets/error_revealer.rs
@@ -1,20 +1,13 @@
-const REVEAL_TIME_SECS: u32 = 2;
-
 use gtk::{glib, prelude::*, subclass::prelude::*};
 
 mod imp {
-    use glib::SourceId;
-    use gtk::CompositeTemplate;
-
-    use std::cell::RefCell;
-
     use super::*;
     use glib::subclass;
+    use gtk::CompositeTemplate;
 
     #[derive(Debug, CompositeTemplate)]
     #[template(resource = "/com/belmoussaoui/Authenticator/error_revealer.ui")]
     pub struct ErrorRevealer {
-        pub source_id: RefCell<Option<SourceId>>,
         #[template_child]
         pub label: TemplateChild<gtk::Label>,
         #[template_child]
@@ -33,7 +26,6 @@ mod imp {
 
         fn new() -> Self {
             Self {
-                source_id: RefCell::default(),
                 label: TemplateChild::default(),
                 revealer: TemplateChild::default(),
             }
@@ -50,14 +42,7 @@ mod imp {
     }
 
     impl ObjectImpl for ErrorRevealer {
-        fn constructed(&self, obj: &Self::Type) {
-            self.parent_constructed(obj);
-        }
-
         fn dispose(&self, _obj: &Self::Type) {
-            if let Some(id) = self.source_id.borrow_mut().take() {
-                glib::source_remove(id);
-            }
             self.revealer.unparent();
             self.label.unparent();
         }
@@ -67,9 +52,7 @@ mod imp {
 }
 
 glib::wrapper! {
-    pub struct ErrorRevealer(
-        ObjectSubclass<imp::ErrorRevealer>)
-        @extends gtk::Widget, gtk::ListBoxRow;
+    pub struct ErrorRevealer(ObjectSubclass<imp::ErrorRevealer>) @extends gtk::Widget;
 }
 
 impl ErrorRevealer {
@@ -82,20 +65,14 @@ impl ErrorRevealer {
         let self_ = imp::ErrorRevealer::from_instance(self);
         self_.label.set_text(text);
 
-        if let Some(id) = self_.source_id.borrow_mut().take() {
-            glib::source_remove(id);
-        }
-
         self_.revealer.set_reveal_child(true);
-        let id = glib::timeout_add_seconds_local(
-            REVEAL_TIME_SECS,
+        glib::timeout_add_seconds_local(
+            2,
             glib::clone!(@weak self as error_revealer => move || {
                 let error_revealer_ = imp::ErrorRevealer::from_instance(&error_revealer);
                 error_revealer_.revealer.set_reveal_child(false);
                 glib::Continue(false)
             }),
         );
-
-        self_.source_id.replace(Some(id));
     }
 }
