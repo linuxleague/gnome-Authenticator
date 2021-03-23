1cd4e0a43a60789387821ea974ecfda4315f592b
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-23 20:05:58 +0100

    bump deps

--- build-aux/cargo.sh
+++ build-aux/cargo.sh
@@ -8,17 +8,14 @@ export OUTPUT="$3"
 export BUILDTYPE="$4"
 export APP_BIN="$5"
 
-
-if [[ $BUILDTYPE = "release" ]]
-then
+if [[ $BUILDTYPE = "release" ]]; then
     echo "RELEASE MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --release && \
+        "$MESON_SOURCE_ROOT"/Cargo.toml --release &&
         cp "$CARGO_TARGET_DIR"/release/"$APP_BIN" "$OUTPUT"
 else
     echo "DEBUG MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --verbose && \
+        "$MESON_SOURCE_ROOT"/Cargo.toml &&
         cp "$CARGO_TARGET_DIR"/debug/"$APP_BIN" "$OUTPUT"
 fi
-
