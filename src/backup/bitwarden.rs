5a0e6f137b10dbc0305c4f820db010bb62dd063e
Julian Hofer <julianhofer@gnome.org>
2021-05-20 22:29:24 +0200

    Allow otpauth://-format authenticator keys for Bitwarden restore

--- src/backup/bitwarden.rs
+++ src/backup/bitwarden.rs
@@ -1,5 +1,7 @@
+use std::str::FromStr;
+
 use super::{Restorable, RestorableItem};
-use crate::models::{Algorithm, OTPMethod};
+use crate::models::{Algorithm, OTPMethod, OTPUri};
 use anyhow::Result;
 use gettextrs::gettext;
 use gtk::prelude::*;
@@ -13,8 +15,19 @@ pub struct Bitwarden {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct BitwardenItem {
-    pub name: Option<String>,
+    #[serde(rename = "name")]
+    pub issuer: Option<String>,
     pub login: Option<BitwardenDetails>,
+    #[serde(skip_serializing)]
+    pub algorithm: Algorithm,
+    #[serde(skip_serializing)]
+    pub method: OTPMethod,
+    #[serde(skip_serializing)]
+    pub digits: Option<u32>,
+    #[serde(skip_serializing)]
+    pub period: Option<u32>,
+    #[serde(skip_serializing)]
+    pub counter: Option<u32>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -37,7 +50,7 @@ impl RestorableItem for BitwardenItem {
     }
 
     fn issuer(&self) -> String {
-        if let Some(issuer) = self.name.clone() {
+        if let Some(issuer) = self.issuer.clone() {
             issuer
         } else {
             gettext("Unknown issuer")
@@ -49,23 +62,46 @@ impl RestorableItem for BitwardenItem {
     }
 
     fn period(&self) -> Option<u32> {
-        None
+        self.period
     }
 
     fn method(&self) -> OTPMethod {
-        OTPMethod::TOTP
+        self.method
     }
 
     fn algorithm(&self) -> Algorithm {
-        Algorithm::SHA1
+        self.algorithm
     }
 
     fn digits(&self) -> Option<u32> {
-        None
+        self.digits
     }
 
     fn counter(&self) -> Option<u32> {
-        None
+        self.counter
+    }
+}
+
+impl BitwardenItem {
+    fn overwrite_with(&mut self, uri: OTPUri) {
+        if self.issuer.is_none() {
+            self.issuer = Some(uri.issuer());
+        }
+
+        if let Some(ref mut login) = self.login {
+            login.totp = Some(uri.secret());
+        } else {
+            self.login = Some(BitwardenDetails {
+                username: None,
+                totp: Some(uri.secret()),
+            });
+        }
+
+        self.algorithm = uri.algorithm();
+        self.method = uri.method();
+        self.digits = uri.digits();
+        self.period = uri.period();
+        self.counter = uri.counter();
     }
 }
 
@@ -89,17 +125,20 @@ impl Restorable for Bitwarden {
         let (data, _) = from.load_contents(gtk::gio::NONE_CANCELLABLE)?;
 
         let bitwarden_root: Bitwarden = serde_json::de::from_slice(&data)?;
-        let items = bitwarden_root
-            .items
-            .into_iter()
-            // Only take the fields where at least the totp secret is present
-            .filter(|item| {
-                item.login
-                    .as_ref()
-                    .and_then(|login| login.totp.as_ref())
-                    .is_some()
-            })
-            .collect();
+
+        let mut items = Vec::new();
+
+        for mut item in bitwarden_root.items {
+            if let Some(ref login) = item.login {
+                if let Some(ref totp) = login.totp {
+                    if let Ok(uri) = OTPUri::from_str(&totp) {
+                        item.overwrite_with(uri);
+                    }
+                    items.push(item);
+                }
+            }
+        }
+
         Ok(items)
     }
 }
