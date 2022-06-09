0cf9a684623874e362fe96c411da02ff372b98c8
Julia Johannesen <julia@insertdomain.name>
2022-06-09 14:07:24 -0400

    Add tests to `src/backup/google.rs`

--- src/backup/google.rs
+++ src/backup/google.rs
@@ -1,4 +1,4 @@
-use super::Restorable;
+use super::{Restorable, RestorableItem};
 use crate::models::{Algorithm, OTPMethod, OTPUri};
 use anyhow::Result;
 use gettextrs::gettext;
@@ -96,7 +96,7 @@ impl Restorable for Google {
 
                         let mut buffer = [0; 128];
 
-                        if let Err(_) = binascii::b32encode(secret, &mut buffer) {
+                        if binascii::b32encode(secret, &mut buffer).is_err() {
                             return folded;
                         }
 
@@ -179,3 +179,25 @@ mod protobuf {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_google_restore_otpauth() {
+        let google_data = b"otpauth-migration://offline?data=CjYKEExyJfPiZeroMa/MdF%2BnkTISE2pvaG5kb2VAZXhhbXBsZS5jb20aB0Rpc2NvcmQgASgBMAIQARgBIAA%3D";
+
+        let google_items = Google::restore_from_data(google_data, None)
+            .expect("Restoring from otpauth-migration uri should work");
+
+        assert_eq!(google_items[0].account(), "johndoe@example.com");
+        assert_eq!(google_items[0].issuer(), "Discord");
+        assert_eq!(google_items[0].secret(), "JRZCL47CMXVOQMNPZR2F7J4RGI");
+        assert_eq!(google_items[0].period(), None);
+        assert_eq!(google_items[0].algorithm(), Algorithm::SHA1);
+        assert_eq!(google_items[0].digits(), None);
+        assert_eq!(google_items[0].counter(), Some(0));
+    }
+}
+
